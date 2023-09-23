97abe6e216c1d7f205a92f26a76435542a2f56e1
Anush Kapoor <121352558+Ank206@users.noreply.github.com>
2023-09-23 23:21:53 +0530

    Logout done

--- dbconnect/src/app/profile/page.tsx
+++ dbconnect/src/app/profile/page.tsx
@@ -1,9 +1,29 @@
+"use client";
 import React from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 const Profile = () => {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      console.log("Logout Succesful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Logout Error");
+      console.log(error.message);
+    }
+  };
+
   return (
     <div>
       <h1 className="font-mono text-2xl m-2">Profile Page</h1>
+      <button className="btn btn-error m-2" onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 };
